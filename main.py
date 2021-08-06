7e4feffbbdfac776ba2993045a857ff49700edda
VictorP03 <83514613+VictorP03@users.noreply.github.com>
2021-08-06 19:01:19 +0200

    new commit

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ import requests
 from pprint import pprint
 from secrets import GITHUB_TOKEN
 import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", '-n', type=str, dest="name", required=True)
+parser.add_argument("--private", '-p', dest="is_private", action="store_true")
+args = parser.parse_args()
+name = args.name
+is_private = args.is_private
 
 API_URL = 'https://api.github.com'
-jData = '{"name":"name"}'
+if is_private:
+    jData = '{"name": "' + name + '", "private": true}'
+else:
+    jData = '{"name": "' + name + '", "private": false}'
+
 headers = {
     "Authorization": "token " + GITHUB_TOKEN,
     "Accept": "application/vnd.github.v3+json"
 }
-r = requests.post(API_URL + "/user/repos", data=jData, headers=headers)
+try:
+    r = requests.post(API_URL + "/user/repos", data=jData, headers=headers)
+    r.raise_for_status()
+except FileExistsError as err:
+    raise SystemExit(err)
 
-pprint(r.json())
+REPO_PATH = 'C:/projecten/'
+os.chdir(REPO_PATH)
+os.system("mkdir " + name)
+os.chdir(REPO_PATH + name)
+os.system("git init")
+os.system("git remote add origin https://github.com/VictorP03/" + name + ".git")
+os.system("echo '# " + name + "' >> README.md")
+os.system("git add .")
+os.system("git commit -m \"first commit\"")
+os.system("git push origin master")
